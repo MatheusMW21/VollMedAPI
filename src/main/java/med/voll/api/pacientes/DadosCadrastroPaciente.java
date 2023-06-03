c442af1ddc24d2010e2fc491b399f7e07137628c
matheus <matheusmw21@gmail.com>
2023-06-03 13:16:33 -0300

    feat: pacient controller

---
+++ src/main/java/med/voll/api/pacientes/DadosCadrastroPaciente.java
@@ -0,0 +1,13 @@
+package med.voll.api.pacientes;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadrastroPaciente(
+        String nome,
+        String email,
+        String telefone,
+        String cpf,
+        DadosEndereco endereco
+) {
+
+}
